c033d81a59a16304568d3b4c5b9423a5fd9f031b
KauaDrar <kauadosreisakamine@hotmail.com>
2023-02-18 13:01:45 -0300

    code refactored with a Promise in order to improve the the photo sending process.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,35 +9,44 @@ class UserController {
   onSubmit() {
     this.formEl.addEventListener("submit", (event) => {
       event.preventDefault();
-
       let values = this.getValues();
+      this.getPhoto().then(
+        (content) => {
+          values.photo = content;
 
-      this.getPhoto((content) => {
-        values.photo = content;
-
-        this.addLine(values);
-      });
+          this.addLine(values);
+        },
+        (e) => {
+          console.error(e);
+        }
+      );
 
       this.addLine(values);
     });
   }
 
-  getPhoto(callback) {
-    let fileReader = new FileReader();
+  getPhoto() {
+    return new Promise((resolve, reject) => {
+      let fileReader = new FileReader();
 
-    let elements = [...this.formEl.elements].filter((item) => {
-      if (item.name === "photo") {
-        return item;
-      }
+      let elements = [...this.formEl.elements].filter((item) => {
+        if (item.name === "photo") {
+          return item;
+        }
+      });
+
+      let file = elements[0].files[0];
+
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+
+      fileReader.onerror = (e) => {
+        reject(e);
+      };
+
+      fileReader.readAsDataURL(file);
     });
-
-    let file = elements[0].files[0];
-
-    fileReader.onload = () => {
-      callback(fileReader.result);
-    };
-
-    fileReader.readAsDataURL(file);
   }
 
   getValues() {
