d7015b492376433e5bb76d2692f92cb907f46ca0
KauaDrar <kauadosreisakamine@hotmail.com>
2023-02-18 12:53:12 -0300

    saving user data in a Class instead a json.

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser) {
-  var tr = document.createElement("tr");
-
-  tr.innerHTML = `
+  console.log(dataUser);
+  var tr = (document.getElementById("table-users").innerHTML = `
       <tr>
           <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
           <td>${dataUser.name}</td>
@@ -16,9 +15,7 @@ function addLine(dataUser) {
               <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
           </td>
       </tr>
-    `;
-
-  document.getElementById("table-users").appendChild(tr);
+      `);
 }
 
 document
@@ -29,11 +26,13 @@ document
     fields.forEach(function (field, index) {
       if (field.name == "gender") {
         if (field.checked) {
-          user.gender;
+            user[field.name] = field.value;
         }
       } else {
         user[field.name] = field.value;
       }
     });
-    addLine(user);
+    var objectUser = new User( user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin );
+
+    addLine(objectUser);
   });
